9969c05c9be5d4e6ee9858451e899ab9e457a44e
Aayushi <aatulasyan143@gmail.com>
2021-04-01 14:59:40 +0530

    basic functionality added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, {useState} from "react";
 
 function App() {
+  
+  const [ currentQuestionOn, setQuestion ] = useState(1);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+
+  const UponButtonClick = (isCorrect) => {
+    if(isCorrect){
+      setScore(score+1);
+    }
+    const nextQuestion = currentQuestionOn + 1;
+    if(nextQuestion< questions.length){
+    setQuestion(nextQuestion);
+    }else{
+      setShowScore(true);
+    }
+  };
+
   const questions = [
     {
       questionText: 'If soccer is called football in England, what is American football called in England?',
       answerOptions: [
-        { answeText: 'American football' , isCorrect: false},
+        { answeText: 'American football' , isCorrect: true},
         { answeText: 'Combball', isCorrect: false},
         { answeText: 'Handball', isCorrect: false},
         { answeText: 'Touchdown', isCorrect: false},
@@ -14,7 +31,7 @@ function App() {
     {
       questionText: 'A doctor with a PhD is a doctor of what?',
       answerOptions: [
-        { answeText: 'Philosophy' , isCorrect: false},
+        { answeText: 'Philosophy' , isCorrect: true},
         { answeText: 'Psychology', isCorrect: false},
         { answeText: 'Phrenology', isCorrect: false},
         { answeText: 'Physical Therapy', isCorrect: false},
@@ -23,7 +40,7 @@ function App() {
     {
       questionText: 'What is the 100th digit of Pi?',
       answerOptions: [
-        { answeText: '9' , isCorrect: false},
+        { answeText: '9' , isCorrect: true},
         { answeText: '4', isCorrect: false},
         { answeText: '7', isCorrect: false},
         { answeText: '2', isCorrect: false},
@@ -32,7 +49,7 @@ function App() {
     {
       questionText: 'An organic compound is considered an alcohol if it has what functional group?',
       answerOptions: [
-        { answeText: 'Hydroxyl' , isCorrect: false},
+        { answeText: 'Hydroxyl' , isCorrect: true},
         { answeText: 'Carbonyl', isCorrect: false},
         { answeText: 'Alkyl', isCorrect: false},
         { answeText: 'Aldehyde', isCorrect: false},
@@ -41,7 +58,7 @@ function App() {
     {
       questionText: 'What is the largest country in the world?',
       answerOptions: [
-        { answeText: 'Russia' , isCorrect: false},
+        { answeText: 'Russia' , isCorrect: true},
         { answeText: 'Canada', isCorrect: false},
         { answeText: 'China', isCorrect: false},
         { answeText: 'United States', isCorrect: false},
@@ -49,9 +66,23 @@ function App() {
     },
   ]
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <div className='app'>
+			{showScore ? (
+				<div className='score-section'>You scored {score} out of {questions.length}</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-text'>{questions[currentQuestionOn].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestionOn].answerOptions.map((answerOption, index) => (
+              	<button onClick={() => UponButtonClick(answerOption.isCorrect)}>{answerOption.answeText}</button>
+            ))}
+					
+					</div>
+				</>
+			)}
+		</div>
   );
 }
 
